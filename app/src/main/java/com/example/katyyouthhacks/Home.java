2c5dba58b07f8cfbcfd49200208bc069a3cabf48
Breanna12 <56575941+Breanna12@users.noreply.github.com>
2023-08-12 00:51:39 -0700

    donate somewhat working commit

--- app/src/main/java/com/example/katyyouthhacks/Home.java
+++ app/src/main/java/com/example/katyyouthhacks/Home.java
@@ -2,13 +2,31 @@ package com.example.katyyouthhacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    private Button swapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        swapButton = (Button) findViewById(R.id.swapBtn);
+        swapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSwap();
+            }
+        });
+    }
+
+    public void openSwap() {
+        Intent intent = new Intent(this, Donate.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/katyyouthhacks/MainActivity.java
+++ app/src/main/java/com/example/katyyouthhacks/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.katyyouthhacks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button homeButton;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeButton = (Button) findViewById(R.id.homeBtn);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHome();
+            }
+        });
+    }
+
+    public void openHome() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
 }
